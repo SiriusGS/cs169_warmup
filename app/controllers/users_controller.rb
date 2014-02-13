f5c4757d8bc658c1adaa3bd2aac42d15eb504086
SiriusGS <gongshi92@gmail.com>
2014-02-13 14:45:46 -0800

    New

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-'''class UsersController < ApplicationController
+class UsersController < ApplicationController
 
 	
 	SUCCESS = 1
@@ -64,65 +64,6 @@
     render :json => output_json, :status =>200
   end
 
-end'''
-class UsersController < ApplicationController
-  def webpage
-  end
-
-  def add
-    if !request.headers["Content-Type"] == "application/json"
-      render :json => {}, :status => 500
-    end
-    my_usr = params[:user]
-    my_passwd = params[:password]
-    return_code = Users.add(my_usr, my_passwd)
-    if return_code < 1
-      err_json = {:errCode => return_code}
-      render :json => err_json, :status => 200
-    else
-      return_json = {:errCode => $SUCCESS, :count => return_code}
-      render :json => return_json, :status => 200
-    end
-  end
-
-  def login
-    if !request.headers["Content-Type"] == "application/json"
-      render :json => {}, :status => 500
-    end
-    my_usr = params[:user]
-    my_passwd = params[:password]
-    return_code = Users.login(my_usr, my_passwd)
-    if return_code < 1
-      err_json = {:errCode => return_code}
-      render :json => err_json, :status => 200
-    else
-      return_json = {:errCode => $SUCCESS, :count => return_code}
-      render :json => return_json, :status =>200
-    end
-  end
-
-  def resetFixture
-    
-    return_code = Users.TESTAPI_resetFixture
-    return_json = {:errCode => return_code}
-    render :json =>return_json, :status => 200
-  end
-
-  def unitTests
-      system("RAILS_ENV=development ruby -Itest test/unit/users_test.rb > test/unit/test_output")
-      system("tail -1 test/unit/test_output > test/unit/out")
-      output = File.open("test/unit/out")
-      output_value = (output.readlines())[0]
-      
-
-       # For reference, the last line of the file looks like:
-       # "10 tests, 17 assertions, 0 failures, 0 errors, 0 skips"
-      failed_tests = Integer(((output_value.split(",")[2]).scan(/\d+/))[0])
-      total_tests = Integer(((output_value.split(",")[0]).scan(/\d+/))[0])
-      output_json = {:totalTests => total_tests, :nrFailed => failed_tests, :output => (File.open("test/unit/test_output")).read}
-      render :json => output_json, :status =>200
-  end
 end
-
 
  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-'''
+
 $SUCCESS = 1
 $ERR_BAD_CREDENTIALS = -1
 $ERR_USER_EXISTS = -2
@@ -41,40 +41,3 @@ class Users < ActiveRecord::Base
 		return $SUCCESS
 	end
 end
-'''
-class Users < ActiveRecord::Base
-	attr_accessible :username, :password, :count
-
-	def self.login(my_usr, my_passwd)
-		login_record = where("username = ? AND password = ?", my_usr, my_passwd)
-		if login_record.length > 0
-			user_record = login_record.first
-			user_record.count = user_record.count + 1
-			user_record.save
-			return user_record.count
-		else 
-			return $ERR_BAD_CREDENTIALS 
-		end
-	end
-
-
-	def self.add(my_usr, my_passwd)
-		if Users.exists?(:username => my_usr.to_s)
-			return $ERR_USER_EXISTS
-		elsif my_usr.nil? or my_usr.to_s == "" or my_usr.length > 128
-			return $ERR_BAD_USERNAME
-		elsif my_passwd.nil? or my_passwd.length > 128
-			return $ERR_BAD_PASSWORD
-		else 
-			new_user_record = Users.new(:username => my_usr, :password => my_passwd, :count => 1)
-			new_user_record.save
-			return new_user_record.count
-		end
-	end
-
-	def self.TESTAPI_resetFixture
-		Users.delete_all
-		return $SUCCESS
-	end
-end
-'''
