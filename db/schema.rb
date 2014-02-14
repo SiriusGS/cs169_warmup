3ff6034d9beda8006232e2ab0673711dcd0454f2
SiriusGS <gongshi92@gmail.com>
2014-02-13 20:05:16 -0800

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140213214019) do
+ActiveRecord::Schema.define(version: 20140214040010) do
 
   create_table "users", force: true do |t|
     t.string   "username"
